ab9f2e70d02373e64c63d2b1b95d0469cd7f4a68
Rafid Aslam (refeed) <rafidteam@gmail.com>
2023-05-11 17:00:07 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,89 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_vpc" "terra_vpc" {
+  cidr_block       = "10.20.0.0/16"
+  tags = {
+    Name = "TerraVPC"
+  }
+}
+
+resource "aws_security_group" "webservers" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.terra_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+# Create a new load balancer
+resource "aws_elb" "terra-elb" {
+  name               = "terra-elb"
+  security_groups = [aws_security_group.webservers.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/index.html"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 100
+  connection_draining         = true
+  connection_draining_timeout = 300
+
+  tags = {
+    Name = "terraform-elb"
+  }
+}
+
+resource "aws_elb" "without_secgroup" {
+  name               = "terra-elb"
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/index.html"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 100
+  connection_draining         = true
+  connection_draining_timeout = 300
+
+  tags = {
+    Name = "terraform-elb"
+  }
+}
+
